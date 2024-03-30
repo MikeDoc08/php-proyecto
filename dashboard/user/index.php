a33d76d2d8b3ff659167e5b85c1e6dd76610f0c4
Alejandro251098 <alejoramirez19@gmail.com>
2024-03-30 10:42:48 -0500

    estrutura hmtl,codigo estilo e imagenes

---
+++ dashboard/user/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DashBoard</title>
+    <link rel="stylesheet" href="user.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="Navegacion">
+        <h2>Nombre de pagina</h2>
+        <ul>
+            <li>Inicio</li>
+            <li>Informacion</li>
+            <li>Academico</li>
+            <li>Configuracion</li>
+        </ul>
+        <button class="boton_cerrar"><h3>Cerrar sesion</h3></button>  
+    </div>
+    <main>
+        <article class="InformacionUsuario">
+            <h2> Bienvenido(a) Usuario</h2> <!--En usuario va el nombre de la Persona -->
+            <img src="FotoSinFondo.jpg.jpg" alt="FondoSinFoto" class="FotoPerfil">
+            <br>
+            <ul>
+                <p>Nombre de curso:</p>
+                <p>Profesor del curso:</p>
+                <p>Semestre:</p>
+                <p>Promedio:</p>
+            </ul>
+        </article>
+        <Aside class="BarraLateral">
+            <ul class="lista_lateral">
+                <li>Horarios</li>
+                <li>Cursos extra</li>
+                <li>Prematriculas</li>
+                <li>Homologaciones</li>  
+            </ul>
+            <img src="background.jpg.jpg" alt="fondoAside" height="360px" width="250px" >
+        </Aside>
+    </main>
+    <footer>
+        <ul class="breadcrumb">
+            <li><a href="https://chat.whatsapp.com/DTsbWT9YP5yDHpBKQZhxnV" target="_blank">Contacto Grupo whatsapp</a></li>
+            <li>Siguenos en nuestras redes sociales</li>
+            <li><a href="" target="_blank">Facebook</a></li>
+            <li><a href=""target="_blank">Instagram</a></li>
+            <li><a href="" target="_blank">Youtube</a></li>
+            <li><a href="" target="_blank">Nuestra pagina</a></li>
+        </ul>
+    </footer>
+
+    
+</body>
+</html>
